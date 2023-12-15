afd5b1d6b0b3935db2dd9457999259071a3aab83
Cinemato <alharerekhaled@gmail.com>
2023-12-15 17:48:07 +0300

    Set up Recycler View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.cinemato.memorygameapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cinemato.memorygameapp"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/cinemato/memorygameapp/MainActivity.kt
+++ app/src/main/java/com/cinemato/memorygameapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.cinemato.memorygameapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvMoves: TextView
+    private lateinit var tvPairs: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvMoves = findViewById(R.id.tvMoves)
+        tvPairs = findViewById(R.id.tvPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
     }
 }
