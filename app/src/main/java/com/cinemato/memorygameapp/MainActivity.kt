b2db4d0022dc82c03432e5ac4ba10f72c2a8f527
Cinemato <alharerekhaled@gmail.com>
2023-12-15 23:39:51 +0300

    Added and displayed images on cards

--- app/src/main/java/com/cinemato/memorygameapp/MainActivity.kt
+++ app/src/main/java/com/cinemato/memorygameapp/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cinemato.memorygameapp.models.BoardSize
+import com.cinemato.memorygameapp.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvMoves: TextView
     private lateinit var tvPairs: TextView
+
+    private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,11 @@ class MainActivity : AppCompatActivity() {
         tvMoves = findViewById(R.id.tvMoves)
         tvPairs = findViewById(R.id.tvPairs)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
+        val randomizedImages = (chosenImages + chosenImages).shuffled()
+
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages)
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this, 2)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 }
---
+++ app/src/main/java/com/cinemato/memorygameapp/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.cinemato.memorygameapp.utils
+
+import com.cinemato.memorygameapp.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_face,
+    R.drawable.ic_baseball,
+    R.drawable.ic_car,
+    R.drawable.ic_cloud,
+    R.drawable.ic_cookie,
+    R.drawable.ic_flower,
+    R.drawable.ic_game,
+    R.drawable.ic_lamp,
+    R.drawable.ic_motor,
+    R.drawable.ic_scissors,
+    R.drawable.ic_volcano,
+    R.drawable.ic_water
+)
